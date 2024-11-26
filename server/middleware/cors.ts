8f7f05233f4496484871d6c45abf33363cf77f78
Revan Kurniawan <rvnkrwn@hotmail.com>
2024-11-26 19:44:32 +0700

    chore and update

---
+++ server/middleware/cors.ts
@@ -0,0 +1,14 @@
+export default defineEventHandler((event) => {
+    const response = event.node.res;
+
+    // Menambahkan header CORS
+    response.setHeader('Access-Control-Allow-Origin', '*');  // Mengizinkan semua asal
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');  // Metode HTTP yang diizinkan
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Header yang diizinkan
+
+    // Jika permintaan preflight (OPTIONS), kirimkan respons 200
+    if (event.req.method === 'OPTIONS') {
+        response.statusCode = 200;
+        response.end();
+    }
+});
