c4a415d9a0a4c700d896a332f176cf8831b7bba1
Revan Kurniawan <rvnkrwn@hotmail.com>
2024-12-02 22:21:50 +0700

    chore and update

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,9 +1,9 @@
 <template>
   <header class="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-3">
     <nav class="max-w-[85rem] w-full mx-auto px-4 flex flex-wrap basis-full items-center justify-between">
-      <a class="sm:order-1 flex-none text-xl font-semibold focus:outline-none focus:opacity-80" href="#">
+      <NuxtLink to="/" class="sm:order-1 flex-none text-xl font-semibold focus:outline-none focus:opacity-80" href="#">
         Dolan Lur.
-      </a>
+      </NuxtLink>
       <div class="sm:order-3 flex items-center gap-x-2">
         <button type="button"
                 class="sm:hidden hs-collapse-toggle relative size-7 flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
